d434a88b088ab9e796a81838ffb4443d067bbfa4
NoNamer777 <oscar.wellner@gmail.com>
2024-09-07 20:15:36 +0200

    Add generated SQL scripts

---
+++ assets/data/sql/insert_product_line_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `Product_Line`(`product_line_code`, `product_line_en`) VALUES
+	(1, 'Camping Equipment'),
+	(2, 'Mountaineering Equipment'),
+	(3, 'Personal Accessories'),
+	(4, 'Outdoor Protection'),
+	(5, 'Golf Equipment');
---
+++ assets/data/sql/insert_return_reason_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `Return_Reason`(`return_reason_code`, `reason_description_en`) VALUES
+	(1, 'Defective product'),
+	(2, 'Incomplete product'),
+	(3, 'Wrong product ordered'),
+	(4, 'Wrong product shipped'),
+	(5, 'Unsatisfactory product');
